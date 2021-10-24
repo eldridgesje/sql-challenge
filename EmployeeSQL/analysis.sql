2af7a54c51e7967481f199d00028a823c562b05f
Stephen Eldridge <eldridge.sje@gmail.com>
2021-10-24 15:45:51 -0500

    Finished data analysis SQL.

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -25,14 +25,48 @@ e.emp_no = j.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+left join dept_emp as j on
+e.emp_no = j.emp_no
+left join departments as d on
+d.dept_no = j.dept_no;
+
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%';
 
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+left join dept_emp as j on
+e.emp_no = j.emp_no
+left join departments as d on
+d.dept_no = j.dept_no
+where d.dept_name = 'Sales';
+
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+left join dept_emp as j on
+e.emp_no = j.emp_no
+left join departments as d on
+d.dept_no = j.dept_no
+where d.dept_name = 'Sales' or d.dept_name = 'Development';
+
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+select last_name, count(emp_no) as "count of last name"
+from employees
+group by last_name
+order by "count of last name" desc;
+
+
